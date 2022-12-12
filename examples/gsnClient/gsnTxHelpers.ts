04c4649a6bb2d22dc25148f2f80f168b35078f63
dongambas <ctmorris@gmail.com>
2022-12-11 18:57:31 -0800

    update light client

---
+++ examples/gsnClient/gsnTxHelpers.ts
@@ -0,0 +1,163 @@
+import { ethers, BigNumber } from "ethers";
+import { RelayRequest } from "./EIP712/RelayRequest";
+import { gsnLightClientConfig } from "./gsnLightClientConfig";
+import { TypedRequestData } from "./EIP712/typedSigning";
+import {
+  PrefixedHexString,
+  GsnTransactionDetails,
+  Address,
+  AccountKeypair,
+} from "./utils";
+
+import {
+  signTypedData,
+  SignTypedDataVersion,
+  recoverTypedSignature,
+} from "@metamask/eth-sig-util";
+
+//TODO: move outside of @opengsn
+
+import relayHubAbi from "@opengsn/common/dist/interfaces/IRelayHub.json";
+import forwarderAbi from "@opengsn/common/dist/interfaces/IForwarder.json";
+
+const calculateCalldataBytesZeroNonzero = (
+  calldata: PrefixedHexString
+): { calldataZeroBytes: number; calldataNonzeroBytes: number } => {
+  const calldataBuf = Buffer.from(calldata.replace("0x", ""), "hex");
+  let calldataZeroBytes = 0;
+  let calldataNonzeroBytes = 0;
+  calldataBuf.forEach((ch) => {
+    ch === 0 ? calldataZeroBytes++ : calldataNonzeroBytes++;
+  });
+  return { calldataZeroBytes, calldataNonzeroBytes };
+};
+
+const calculateCalldataCost = (
+  msgData: PrefixedHexString,
+  gtxDataNonZero: number,
+  gtxDataZero: number
+): number => {
+  const { calldataZeroBytes, calldataNonzeroBytes } =
+    calculateCalldataBytesZeroNonzero(msgData);
+  return (
+    calldataZeroBytes * gtxDataZero + calldataNonzeroBytes * gtxDataNonZero
+  );
+};
+
+export const estimateGasWithoutCallData = (
+  transaction: GsnTransactionDetails,
+  gtxDataNonZero: number,
+  gtxDataZero: number
+) => {
+  const originalGas = transaction.gas;
+  const callDataCost = calculateCalldataCost(
+    transaction.data,
+    gtxDataNonZero,
+    gtxDataZero
+  );
+  const adjustedgas = BigNumber.from(originalGas).sub(callDataCost);
+  return adjustedgas.toHexString();
+};
+
+export const estimateCalldataCostForRequest = async (
+  relayRequestOriginal: RelayRequest,
+  config: gsnLightClientConfig
+): Promise<PrefixedHexString> => {
+  // protecting the original object from temporary modifications done here
+  const relayRequest = Object.assign({}, relayRequestOriginal, {
+    relayData: Object.assign({}, relayRequestOriginal.relayData),
+  });
+
+  relayRequest.relayData.transactionCalldataGasUsed = "0xffffffffff";
+  relayRequest.relayData.paymasterData =
+    "0x" + "ff".repeat(config.maxPaymasterDataLength);
+  const maxAcceptanceBudget = "0xffffffffff";
+  const signature = "0x" + "ff".repeat(65);
+  const approvalData = "0x" + "ff".repeat(config.maxApprovalDataLength);
+
+  const relayHub = new ethers.Contract(config.relayHubAddress, relayHubAbi);
+
+  const { data } = await relayHub.populateTransaction.relayCall(
+    config.domainSeparatorName,
+    maxAcceptanceBudget,
+    relayRequest,
+    signature,
+    approvalData
+  );
+
+  if (!data) {
+    throw "tx data undefined";
+  }
+
+  return BigNumber.from(
+    calculateCalldataCost(data, config.gtxDataNonZero, config.gtxDataZero)
+  ).toHexString();
+};
+
+export const getSenderNonce = async (
+  sender: Address,
+  forwarderAddress: Address,
+  provider: ethers.providers.JsonRpcProvider
+): Promise<string> => {
+  const forwarder = new ethers.Contract(
+    forwarderAddress,
+    forwarderAbi,
+    provider
+  );
+  const nonce = await forwarder.getNonce(sender);
+  return nonce.toString();
+};
+
+export const signRequest = async (
+  relayRequest: RelayRequest,
+  domainSeparatorName: string,
+  chainId: string,
+  account: AccountKeypair
+) => {
+  const cloneRequest = { ...relayRequest };
+
+  const signedData = new TypedRequestData(
+    domainSeparatorName,
+    Number(chainId),
+    relayRequest.relayData.forwarder,
+    cloneRequest
+  );
+
+  const signature = signTypedData({
+    privateKey: Buffer.from(account.privateKey.replace(/^0x/, ""), "hex"),
+    data: signedData,
+    version: SignTypedDataVersion.V4,
+  });
+
+  const rec = recoverTypedSignature({
+    data: signedData,
+    signature,
+    version: SignTypedDataVersion.V4,
+  });
+
+  return signature;
+};
+
+export const getRelayRequestID = (
+  relayRequest: any,
+  signature: PrefixedHexString = "0x"
+): PrefixedHexString => {
+  const types = ["address", "uint256", "bytes"];
+  const parameters = [
+    relayRequest.request.from,
+    relayRequest.request.nonce,
+    signature,
+  ];
+
+  const hash = ethers.utils.keccak256(
+    ethers.utils.defaultAbiCoder.encode(types, parameters)
+  );
+  const rawRelayRequestId = hash.replace(/^0x/, "").padStart(64, "0");
+
+  const prefixSize = 8;
+  const prefixedRelayRequestId = rawRelayRequestId.replace(
+    new RegExp(`^.{${prefixSize}}`),
+    "0".repeat(prefixSize)
+  );
+  return `0x${prefixedRelayRequestId}`;
+};
