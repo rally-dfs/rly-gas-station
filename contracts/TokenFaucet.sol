e21c3153e396f44f52412dd03ac7224cfe21cbb3
dongambas <ctmorris@gmail.com>
2023-01-06 11:47:22 -0800

    add claim event

--- contracts/TokenFaucet.sol
+++ contracts/TokenFaucet.sol
@@ -6,6 +6,8 @@ import "./gERC20.sol";
 contract TokenFaucet is gERC20 {
     uint256 TOKEN_CLAIM_ALLOWANCE = 10 ether;
 
+    event Claim(address sender, uint256 amount);
+
     constructor(
         string memory _name,
         string memory _symbol,
@@ -18,6 +20,7 @@ contract TokenFaucet is gERC20 {
     function claim() public returns (bool) {
         address sender = _msgSender();
         _mint(sender, TOKEN_CLAIM_ALLOWANCE);
+        emit Claim(sender, TOKEN_CLAIM_ALLOWANCE);
         return true;
     }
 
