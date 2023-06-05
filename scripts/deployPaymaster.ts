040d4f645e5cb3617986eb0ebe67c60231cc68e5
dongambas <ctmorris@gmail.com>
2023-06-05 17:46:24 -0400

    update to beta3

--- scripts/deployPaymaster.ts
+++ scripts/deployPaymaster.ts
@@ -1,13 +1,13 @@
 import hre from "hardhat";
-import { ethers } from "ethers";
 
 require("dotenv").config();
 
 async function main() {
-  const rlyTokenAddress = "0x76b8D57e5ac6afAc5D415a054453d1DD2c3C0094";
+  const rlyTokenAddress = "0x1C7312Cb60b40cF586e796FEdD60Cf243286c9E9";
   const forwarderAddress = "0xB2b5841DBeF766d4b521221732F9B618fCf34A87";
   const relayHubAddress = "0xfCEE9036EDc85cD5c12A9De6b267c4672Eb4bA1B";
   const faucetAddress = "0x78a0794Bb3BB06238ed5f8D926419bD8fc9546d8";
+  const pmA = "0x61B9BdF9c10F77bD9eD033559Cec410427aeb8A2";
 
   const Faucet = await hre.ethers.getContractFactory("TokenFaucet");
   const Token = await hre.ethers.getContractFactory("posRLYTestERC20");
@@ -18,6 +18,7 @@ async function main() {
   // deploy paymaster
 
   const Paymaster = await hre.ethers.getContractFactory("RLYPaymaster");
+
   const methodIdClaim = faucet.interface.getSighash("claim");
   const methodIdTransfer = token.interface.getSighash("transfer");
   const methodIdExecute = token.interface.getSighash("executeMetaTransaction");
@@ -29,7 +30,7 @@ async function main() {
   await pm.setRelayHub(relayHubAddress!);
   await pm.setTrustedForwarder(forwarderAddress!);
 
-  console.log("paymaster deployed to", pm.address);
+  const version = await pm.versionPaymaster();
 }
 
 main().catch((error) => {
