70fb8e4ec260f11f9de63b0b93115aabfdf761be
dongambas <ctmorris@gmail.com>
2023-06-14 19:04:52 -0700

    remove logging

--- scripts/deployLocal.ts
+++ scripts/deployLocal.ts
@@ -5,7 +5,6 @@ const relayHubAbi = require("@opengsn/common/dist/interfaces/IRelayHub.json");
 
 async function main() {
   const env = await GsnTestEnvironment.startGsn("localhost", 8090);
-  console.log(env.relayUrl);
   const { contractsDeployment } = env;
 
   const accounts = await hre.ethers.getSigners();
